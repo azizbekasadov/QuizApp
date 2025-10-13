0ea094f7bf3b25f7965a9741b6b65b65f8037870
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-10-13 14:37:33 +0200

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Azizbek Asadov on 13.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
